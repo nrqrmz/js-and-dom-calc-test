6b7326d1341e8e56af62ad56411dfb8248db77ea
Enrique Ramirez <nrqrmz@gmail.com>
2024-05-26 22:29:11 -0500

    add input to display on click

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,20 @@
+// 1. Write operations on the calculator
+
+// 2. Clear the display when clicking on 'C'
+// 3. Compute operations when clicking on '='
+
+// 1.1 Create a selector for the display
 const display = document.querySelector('input')
 
-display.value = 'hello from calculator'
+// 1.2 Create selectors for the buttons
+const buttons = document.querySelectorAll('a')
+
+// 1.3 Add event listeners to the buttons
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    // 1.4 Add clicked button content to the display
+    display.value += button.innerText
+  })
+})
