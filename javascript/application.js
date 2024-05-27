a69e4384fd5c9af9bdd9c59b9620e1d5960d3b41
Enrique Ramirez <nrqrmz@gmail.com>
2024-05-26 22:32:25 -0500

    add clearDisplay function

--- javascript/application.js
+++ javascript/application.js
@@ -1,8 +1,11 @@
-// 1. Write operations on the calculator
-
 // 2. Clear the display when clicking on 'C'
+// 2.1 Define a clearDisplay function
+const clearDisplay = () => {
+  display.value = ''
+}
 // 3. Compute operations when clicking on '='
 
+// 1. Write operations on the calculator
 // 1.1 Create a selector for the display
 const display = document.querySelector('input')
 
@@ -14,7 +17,12 @@ buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
     event.preventDefault()
 
-    // 1.4 Add clicked button content to the display
-    display.value += button.innerText
+    if (button.innerText == 'C') {
+      // 2.2 Call clearDisplay function
+      clearDisplay()
+    } else {
+      // 1.4 Add clicked button content to the display
+      display.value += button.innerText
+    }
   })
 })
